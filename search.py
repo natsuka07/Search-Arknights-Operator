abbf07dc0f7ab0af02439a04729cdfc0b354254a
Friertent <76434365+natsuka07@users.noreply.github.com>
2025-11-13 13:24:52 +0700

    Enhance search functionality and menu structure
    Refactor search function and improve user input handling.

--- search.py
+++ search.py
@@ -1,26 +1,82 @@
-print(" ")
+import os
+from tabulate import tabulate
 import pandas as pd
-dataset = pd.read_csv(r"D:\Code\Tugas\arknights_operators_dataset.csv")
+dataset = pd.read_csv(r"D:\Code\Tugas\arknights_operators_dataset.csv", sep=";")
+
 # ===============================================
-print("Arknights type shit".center(40, "="))
+dataset["Limited"] = dataset["Limited"].fillna("Non-Limited")
+dataset = dataset.drop(["DP Cost", "Block", "Event", "EN_Release"],axis=1)
 
-nama = str(input("Masukkan nama operator : ").strip())
-rarity = str(input("Masukkan rarity operator : ").strip())
+# ===============================================
+def main():
+    bersih()
+    while True:
+        menu()
+        option = input("Masukkan Opsi\n>> ").lower()
+        match option:
+            case "a":
+                bersih()
+                cari()
+            case "b":
+                bersih()
+                gacha()
+            case "c":
+                bersih()
+                tampil()
+            case _:
+                bersih()
+                print("Masukkan input dengan benar\n\n")
+                continue
 
+def cari():
+    welcome("Pencarian operator")
+    nama = str(input("Masukkan nama operator : ").strip())
+    rarity = str(input("Masukkan rarity operator : ").strip())
+    def search(df, nama, rarity):
+        nama = nama.lower()
 
-def search(df, nama, rarity):
-    nama = nama.lower()
+        hasil = df[
+            (df['Name'].str.lower().str.contains(nama, na=False)) &
+            (df['Rarity'].astype(str) == rarity)
+        ]
 
-    hasil = df[
-        (df['Name'].str.lower().str.contains(nama, na=False)) &
-        (df['Rarity'].astype(str) == rarity)
-    ]
+        if not hasil.empty:
+            print("\nOperator(s) found:")
+            print(tabulate(hasil, headers="keys", tablefmt="fancy_grid", showindex=False))
+            # print(hasil.to_string(index=False))
+        else:
+            print(f"\nNo operator found with name containing '{nama}' and rarity '{rarity}'.")
+    search(dataset, nama, rarity)
+    input()
+    bersih()
 
-    if not hasil.empty:
-        print("\nOperator(s) found:")
-        print(hasil.to_string(index=False))  # Display results without index
-    else:
-        print(f"\nNo operator found with name containing '{nama}' and rarity '{rarity}'.")
+def gacha():
+    welcome("Gacha operator")
+    print("I have no idea")
+    input()
+    bersih()
 
-search(dataset, nama, rarity)
+def tampil():
+    print(dataset.head())
+    print("\n\nDataset last updated 12 November 2025")
+    input()
 
+# ===============================================
+def bersih():
+    os.system("cls")
+
+def welcome(args):
+    print("=" * 40)
+    print("=", args.center(36, " "), "=")
+    print("=" * 40)
+
+def menu():
+    print("Mau ngapain?")
+    print("A. Pencarian Operator")
+    print("B. Gacha")
+    print("C. Tampilkan Dataset")
+    print(" ")
+
+# ===============================================
+if __name__ == "__main__":
+    main()
